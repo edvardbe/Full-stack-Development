7af206c0e69194ae8ce8adcf864a5718612a0508
Edvard <edvardee@stud.ntnu.no>
2025-01-12 03:02:29 +0100

    Add log

--- Course Work/e2/src/App.vue
+++ Course Work/e2/src/App.vue
@@ -1,26 +1,35 @@
 <template>
   <main>
     <div class="calculator">
-      <div class="display">{{ displayValue }}
-      </div>
+      <div class="display">{{ displayValue }}</div>
       <div class="keys">
-        <button class="operator" @click="clearDisplay">C</button><button class="operator">ANS</button><button class="operator">DEL</button><button class="operator" @click="handleInput('+')">+</button>
+        <button class="operator" @click="clearDisplay()">C</button><button class="operator" @click="ans()">ANS</button><button class="operator">DEL</button><button class="operator" @click="handleInput('+')">+</button>
         <button @click="handleInput('1')">1</button><button @click="handleInput('2')">2</button><button @click="handleInput('3')">3</button><button class="operator" @click="handleInput('-')">-</button>
         <button @click="handleInput('4')">4</button><button @click="handleInput('5')">5</button><button @click="handleInput('6')">6</button><button class="operator" @click="handleInput('*')">*</button>
-        <button @click="handleInput('7')">7</button><button @click="handleInput('8')">8</button><button @click="handleInput('7')">9</button><button class="operator" @click="handleInput('/')">/</button>
-        <button class="operator"></button><button @click="handleInput('0')">0</button><button class="operator" @click="handleInput('.')">.</button><button class="equals" @click="calculateResult()">=</button>
+        <button @click="handleInput('7')">7</button><button @click="handleInput('8')">8</button><button @click="handleInput('9')">9</button><button class="operator" @click="handleInput('/')">/</button>
+        <button class="operator" @click="suprise()"></button><button @click="handleInput('0')">0</button><button class="operator" @click="handleInput('.')">.</button><button class="equals" @click="calculateResult()">=</button>
+      </div>
+    </div>
+    <div class="log-container">
+      <button class="collapsible" :class="{collapsed: isCollapsed, revealed: !isCollapsed}" @click="toggleCollapse">{{ isCollapsed ? 'Show Log' : 'Hide Log' }}</button>
+      <div class="content" v-show="!isCollapsed">
+        <pre id="log" class=log></pre>
+        <button @click="clearLog()"><span>&#128465;</span>Clear log</button>q
+        
       </div>
     </div>
   </main>
 </template>
 
 <script>
-import { handleInput, calculateResult, clearDisplay } from './logic/calculatorLogic.js';
+import { handleInput, calculateResult, clearDisplay, suprise, setPrevAns, clearLog} from './logic/calculatorLogic.js';
 
 export default {
   data() {
     return {
       displayValue: '0',
+      prevAns: '0',
+      isCollapsed: true, // Initial state
     };
   },
   methods: {
@@ -29,9 +38,22 @@ export default {
     },
     calculateResult() {
       this.displayValue = calculateResult(this.displayValue);
+      this.prevAns = setPrevAns(this.prevAns, this.displayValue);
+    },
+    ans(){
+      this.displayValue = handleInput(this.displayValue, this.prevAns);
     },
     clearDisplay() {
       this.displayValue = clearDisplay();
+    },
+    suprise(){
+      this.displayValue = suprise(this.displayValue)
+    },
+    toggleCollapse() {
+      this.isCollapsed = !this.isCollapsed; // Toggle the state
+    },
+    clearLog(){
+      clearLog();
     }
   }
 };
--- Course Work/e2/src/logic/calculatorLogic.js
+++ Course Work/e2/src/logic/calculatorLogic.js
@@ -1,24 +1,66 @@
-export function handleInput(currentValue, input){
-    if(currentValue === '0' || currentValue === 'Error'){
-        return input;
+var msgs = ['Error', 'Undefined', ':)'];
+var operators = ['.', '+', '-', '/', '*'];
+
+export function handleInput(prevInputs, input){
+    let logMsg = input;
+    if(operators.indexOf(input) > 0){
+        logMsg = " " + input + " ";
     }
-    else {
-        return currentValue + input;
+    logger(logMsg);
+    if (msgs.indexOf(prevInputs) >= 0 || (prevInputs === '0' && operators.indexOf(input) < 0)){
+        return input;
+    } else {
+        return prevInputs + input;
     }
 }
 
 export function calculateResult(currentValue){
     try{
-        let ret = eval(currentValue).toString();
-        if (ret === 'Infinity'){
-            return 'Undefined';
+        if(msgs.indexOf(currentValue) >= 0){
+            return '0';
         }
+        const fracs = currentValue.split('/');
+        const nFracs = fracs.length;
+        let ret = eval(currentValue).toString();
+
+        if (fracs.slice(1, nFracs).some(frac => eval(frac).toString() === '0')) {
+            ret = 'Undefined';
+        }
+        logger(" = " + ret + "\n");
         return ret;
     } catch{
         return 'Error';
     }
 }
 
+export function setPrevAns(prevAns, newAns){
+    if(msgs.indexOf(newAns) >= 0 || operators.indexOf(newAns) >= 0){
+        return prevAns;
+    }
+    return newAns;
+}
+
 export function clearDisplay(){
+    logger("\nCLEAR\n");
     return '0';
+}
+
+export function logger(prevAns){
+    let curElement = document.getElementById("log");
+    let text = curElement.textContent || curElement.innerText;
+    curElement.textContent = text + prevAns;
+}
+
+export function clearLog(){
+    let curElement = document.getElementById("log");
+    let text = curElement.textContent || curElement.innerText;
+    curElement.textContent = "";
+}
+
+export function suprise(currentValue){
+    if (currentValue === '6969'){
+        window.open('https://www.youtube.com/watch?v=nat2-OTTXyU', "_blank")
+        return ':)';
+    }
+    return currentValue;
 }
